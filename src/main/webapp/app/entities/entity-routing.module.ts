285da64ee2bd51125b19bed3ba1340735898e230
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-12-21 07:46:49 +0100

    init

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'products',
+        data: { pageTitle: 'storeApp.products.home.title' },
+        loadChildren: () => import('./products/products.module').then(m => m.ProductsModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
